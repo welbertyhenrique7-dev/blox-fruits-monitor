ef9b03eb193ae4fb144dcb7d72f65115e0d333fa
welbertyhenrique7-dev <welbertyhenrique7@gmail.com>
2026-08-17 01:10:28 -0400

    Fix import statement and update string quotes

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
 import os
-from flask import
+from flask import Flask
 
 app = Flask(__name__)
 
-TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
-TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
+TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
+TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
 
-@app.route('/')
+@app.route("/")
 def index():
     return "Bot is running!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    
